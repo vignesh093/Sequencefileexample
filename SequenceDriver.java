c74a1f6d8a1b706e38dffbf67855f24e7dbc2aee
vignesh093 <vignesh093@users.noreply.github.com>
2015-05-17 17:00:13 +0530

    Create SequenceDriver.java

---
+++ SequenceDriver.java
@@ -0,0 +1,39 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+
+public class SequenceDriver {
+	public static void main(String args[]) throws Exception
+	{
+		if(args.length!=2)
+			{
+			System.err.println("Usage: Worddrivernewapi <input path> <output path>");
+			System.exit(-1);
+			}
+		Job job=new Job();
+		job.setJarByClass(SequenceDriver.class);
+		job.setJobName("SequenceDriver");
+		
+		FileInputFormat.addInputPath(job,new Path(args[0]));
+		FileOutputFormat.setOutputPath(job,new Path(args[1]));
+		
+		job.setMapperClass(SequenceMapper.class);
+		
+		job.setNumReduceTasks(1);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
+	}
+
+}	
